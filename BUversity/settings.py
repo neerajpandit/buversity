ea36b08d26a28fa3c10a29cf1d1e2a42f5dffbd7
Neerajpandey1507 <pandeyneeraj1507@gmail.com>
2022-12-16 22:19:29 +0530

    Fi

--- BUversity/settings.py
+++ BUversity/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e76p(@xd9%%$us&h8ax@o&#!m82c*n28!g!ee+j@4#7mn**fvn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,6 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn reportersprj.wsgi --log-file--
